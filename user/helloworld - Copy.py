bb42336ea7e1a895fb9214106b7f23b123b66181
Erik Koopmans <koopmans.erik.m@edumail.vic.gov.au>
2018-02-15 12:31:33 +1100

    MCSHANE, Thomas: 2018-02-15 12:31:33

---
+++ user/helloworld - Copy.py
@@ -0,0 +1,9 @@
+name=input('What is your name? ' )
+print(name)
+print('Hello' + " " + name + '! My name is Thomas!')
+you=input('How are you? ' )
+print(you + '?')
+if(you)=good
+print('Thats Good')
+if(you)=bad
+print('Thats Unfortunate')
