53c37c47488c2667ea65cb3950002967401aa9d8
Erik Koopmans <koopmans.erik.m@edumail.vic.gov.au>
2018-03-01 12:30:12 +1100

    MCSHANE, Thomas: 2018-03-01 12:30:12

---
+++ user/MEOW.py
@@ -0,0 +1,9 @@
+import webbrowser
+import time
+x=10
+while x > 0:
+    print (x), time.sleep(1)
+    x=x-1
+while x==0:
+    print ("MEOW")
+    webbrowser.open("https://www.youtube.com/watch?v=IuysY1BekOE")
---
+++ user/tacticalnuke.py
@@ -0,0 +1,6 @@
+import os
+x=0
+while x == 0:
+    print ('TACTICAL NUKE INCOMING')
+    os.system("start D:/BEWARE/lol.py")
+    
