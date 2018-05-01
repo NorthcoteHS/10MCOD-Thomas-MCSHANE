4b61b19c985c0420faaa8678022e934583c0c2e8
ThomasMcshane <mcs0001@nhs.vic.edu.au>
2018-05-02 08:22:09 +1000

    Work on naming module

--- modules/u3_organisationAndReources/naming/New folder/debugMe.py
+++ modules/u3_organisationAndReources/naming/New folder/debugMe.py
@@ -6,19 +6,19 @@ Desc:   Answers yes or no to any question.
 """
 
 # Ask the user for a question.
-question = print('Ask me anything!')
+question = input('Ask me anything!')
 
 # Check for special input.
-if  question = 'Quit'
-    print('Goodbye.)
-else if question == 'Hi' or 'Hello':
+if  question = 'Quit':
+    print('Goodbye.')
+elif question == 'Hi' or 'Hello':
     print("What's up?")
 
 # Answer yes or no randomly.
 else:
 num = random.randint(0,4);
-if num == "1":
+if num == 1:
     print("Yes!")
 if num = 2:
-    "No"
+    print("No")
 done
