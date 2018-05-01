3ae3160e0977fff5bb57b5bab6711fed3b549ca3
ThomasMcshane <mcs0001@nhs.vic.edu.au>
2018-05-01 10:52:17 +1000

    Commiting Everything

--- modules/u3_organisationAndReources/naming/New folder/temp/todo/commentMe.py
+++ modules/u3_organisationAndReources/naming/New folder/temp/todo/commentMe.py
@@ -1,10 +1,17 @@
+"""
+Prog: Circle Calculator
+Name: Thomas Mc
+Date: 1/05/2018
+Desc: Calculate area and perimeter of a circle
+"""
 print('Welcome to the Circle Calculator!')
-
+# Welcome User
 r = input('Enter a radius: ')
 r = int(r)
-
+# Ask User for Radius
 area = 3.14 * r * r
 print('The area is', area)
-
+# Calculate and print are
 perimeter = 3.14 * r * 2
 print('The perimeter is', perimeter)
+# Calculate and print perimeter
--- user/tacticalnuke.py
+++ user/tacticalnuke.py
@@ -3,4 +3,3 @@ x=0
 while x == 0:
     print ('TACTICAL NUKE INCOMING')
     os.system("start D:/BEWARE/lol.py")
-    
